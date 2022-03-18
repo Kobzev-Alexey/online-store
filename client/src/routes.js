c053240cd3ca7fa8bd5360c33b2dbe66cf4f4298
Alexey <lex91179@mail.ru>
2022-03-18 23:27:38 +0300

    mobx

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,7 +3,14 @@ import Auth from "./pages/Auth";
 import Basket from "./pages/Basket";
 import DevicePage from "./pages/DevicePage";
 import Shop from "./pages/Shop";
-import { ADMIN_ROUTE, BASKET_ROUTE, DEVICE_ROUTE, LOGIN_ROUTE, REGISTRATION_ROUTE, SHOP_ROUTE } from "./utills/consts";
+import {
+  ADMIN_ROUTE,
+  BASKET_ROUTE,
+  DEVICE_ROUTE,
+  LOGIN_ROUTE,
+  REGISTRATION_ROUTE,
+  SHOP_ROUTE,
+} from "./utills/consts";
 
 export const authRoutes = [
   {
@@ -30,7 +37,7 @@ export const publicRoutes = [
     Component: Auth,
   },
   {
-    path: DEVICE_ROUTE + '/:id',
+    path: DEVICE_ROUTE + "/:id",
     Component: DevicePage,
   },
 ];
