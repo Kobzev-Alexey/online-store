cb8a4e687318e1650acd2a4eeacf8593efcc6890
Alexey <lex91179@mail.ru>
2022-03-29 18:04:17 +0300

    modal

--- client/src/components/BrandBar.js
+++ client/src/components/BrandBar.js
@@ -1,6 +1,6 @@
 import { observer } from "mobx-react-lite";
 import React, { useContext } from "react";
-import { Card, Col, Row } from "react-bootstrap";
+import { Card, Row } from "react-bootstrap";
 import { Context } from "../index";
 
 const BrandBar = observer(() => {
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,11 +1,12 @@
 import React, { useContext } from "react";
 import { Context } from "../index";
 import { Button, Container, Nav, Navbar } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
-import { SHOP_ROUTE } from "../utills/consts";
+import { NavLink, useNavigate } from "react-router-dom";
+import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from "../utills/consts";
 
 const NavBar = () => {
   const { user } = useContext(Context);
+  const navigate = useNavigate();
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
@@ -15,14 +16,28 @@ const NavBar = () => {
 
         {user.isAuth ? (
           <Nav className="ml-auto" style={{ color: "white" }}>
-            <Button variant={"outline-light"}>Админ панель</Button>
-            <Button variant={"outline-light"} className="ms-2">
-              Войти
+            <Button
+              variant={"outline-light"}
+              onClick={() => navigate(ADMIN_ROUTE)}
+            >
+              Админ панель
+            </Button>
+            <Button
+              variant={"outline-light"}
+              onClick={() => navigate(LOGIN_ROUTE)}
+              className="ms-2"
+            >
+              Выйти
             </Button>
           </Nav>
         ) : (
           <Nav>
-            <Button variant={"outline-light"} onClick={() => user.setIsAuth(true)}>Авторизация</Button>
+            <Button
+              variant={"outline-light"}
+              onClick={() => user.setIsAuth(true)}
+            >
+              Авторизация
+            </Button>
           </Nav>
         )}
       </Container>
